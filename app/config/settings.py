2bfee0059de9be1130d9224dcabc2f399c71577e
Julia Astankina <astankina.julia@gmail.com>
2023-04-21 00:40:56 +0300

    add logging

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,10 +1,12 @@
+import logging.config
 import os
 from datetime import timedelta
-from pathlib import Path
+
+from config import logger
 
 
 class Config(object):
-    """Base configuration."""
+    """ Base configuration. """
 
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
@@ -13,34 +15,43 @@ class Config(object):
 
     DB_TYPE = os.environ.get('DB_TYPE', 'postgresql')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_PORT = os.environ.get('DB_TYPE', '5439')
+    DB_PORT = os.environ.get('DB_TYPE', '5440')
     DB_NAME = os.environ.get('POSTGRES_DB', 'users_db')
     DB_USER = os.environ.get('POSTGRES_USER', 'app')
     DB_PASS = os.environ.get('POSTGRES_PASSWORD', '12345')
 
     SQLALCHEMY_DATABASE_URI = f'{DB_TYPE}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-    JWT_COOKIE_SECURE = os.environ.get('JWT_COOKIE_SECURE', True) == 'True'
+    JWT_COOKIE_SECURE = os.environ.get('JWT_COOKIE_SECURE', False) == 'True'
+    JWT_COOKIE_CSRF_PROTECT = os.environ.get('JWT_COOKIE_CSRF_PROTECT', False) == 'True'
     JWT_TOKEN_LOCATION = os.environ.get('JWT_TOKEN_LOCATION', 'cookies')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'aaa-test')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
+    logging_level = os.environ.get('logging_level', 'INFO')
+    secret_key = os.environ.get('secret_key', 'bbb-test')
+
 
 class ProdConfig(Config):
-    """Production configuration."""
+    """ Production configuration. """
     ENV = 'prod'
     DEBUG = False
 
 
 class DevConfig(Config):
-    """Development configuration."""
+    """ Development configuration. """
     ENV = 'dev'
     DEBUG = True
 
 
 class TestConfig(Config):
-    """ Тестовая конфигурация. """
+    """ Test configuration. """
     ENV = 'test'
     TESTING = True
     DEBUG = True
+
+
+# Set logging settings
+logging.config.dictConfig(logger.LOGGING_SETTINGS)
+logging.getLogger().setLevel(Config().logging_level)
