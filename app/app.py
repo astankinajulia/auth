ca6d7272e1e8e54afa5aa2c3e5917a16343945ce
Julia Astankina <astankina.julia@gmail.com>
2023-04-21 00:43:04 +0300

    add update user, fix api

--- app/app.py
+++ app/app.py
@@ -1,20 +1,40 @@
+import logging
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_login import LoginManager
 
+from db.db_models import get_user
 from users.users import user_bp
-from settings import Config
+from config.settings import Config
+
+log = logging.getLogger(__name__)
 
 
 def create_app():
+    log.info('Create app...')
+
     app = Flask(__name__)
     app.config.from_object(Config)
-    from db import db
+
+    from db.db import db
 
     db.init_app(app)
     app.app_context().push()
     db.create_all()
 
-    app.register_blueprint(user_bp, url_prefix='/user')
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'user_bp.login'
+
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return get_user(id)
+
+    app.register_blueprint(user_bp, url_prefix='')
     jwt = JWTManager(app)
 
     return app
--- app/users/users.py
+++ app/users/users.py
@@ -1,13 +1,31 @@
+import logging
+
 from flask import Blueprint, request, abort, Response, jsonify
-from flask_jwt_extended import create_access_token, set_access_cookies, unset_jwt_cookies, create_refresh_token, \
-    jwt_required, get_jwt_identity
-from flask_login import login_required
-from flask_security import logout_user, login_user
+from flask_jwt_extended import (
+    create_access_token, set_access_cookies, unset_jwt_cookies, create_refresh_token,
+    jwt_required, get_jwt_identity, verify_jwt_in_request
+)
+from flask_login import login_required, login_user
 
-from users.utils import create_user, get_user, validate_email
+from db.errors import NotFoundInDBError
+from db.redis_service import Redis
+from db.user_service import user_service_db
+from errors import NotFoundError, BadRequestError
+from users.utils import validate_email
 
+log = logging.getLogger(__name__)
 
 user_bp = Blueprint('user_bp', __name__)
+
+
+@user_bp.errorhandler(NotFoundError)
+def invalid_api_usage(e):
+    return jsonify(e.to_dict()), e.status_code
+
+
+@user_bp.errorhandler(BadRequestError)
+def invalid_api_usage(e):
+    return jsonify(e.to_dict()), e.status_code
 
 
 @user_bp.route('/register', methods=['POST'])
@@ -17,22 +35,24 @@ def register():
     password = params.get('password')
 
     if not email:
-        abort(400, description='Request without email')
+        raise BadRequestError(message='Request without email')
+        # abort(400, description='Request without email')
     if not password:
         abort(400, description='Request without password')
     if not validate_email(email):
         abort(400, description='Not valid email')
 
-    user = get_user(email)
+    user = user_service_db.get_user_by_email(email)
     if user:
         abort(400, description='User has already registered')
 
-    create_user(email=email, password=password)
+    user_service_db.create_user(email=email, password=password)
     return Response('', status=201, mimetype='application/json')
 
 
 @user_bp.route('/login', methods=['POST'])
 def login():
+    log.info('Login')
     params = request.get_json()
     email = params.get('email')
     password = params.get('password')
@@ -41,46 +61,95 @@ def login():
     if not password:
         abort(400, description='Request without password')
 
-    user = get_user(email)
-    if user and user.verify_password(password):
-        login_user(user)
-        response = jsonify({'msg': 'login successful'})
-
-        access_token = create_access_token(identity=user.id, fresh=True)
-        refresh_token = create_refresh_token(identity=user.id)
-
-        set_access_cookies(response, access_token)
-
-        return jsonify(access_token=access_token, refresh_token=refresh_token)
-    else:
+    try:
+        user = user_service_db.get_user_by_email(email=email, is_optional=False)
+    except NotFoundInDBError:
+        raise NotFoundError(message='User not found')
+    if not user.verify_password(password):
         return jsonify({'msg': 'Bad username or password'}), 401
+
+    login_user(user)
+
+    access_token = create_access_token(identity=user.id, fresh=True)
+    refresh_token = create_refresh_token(identity=user.id)
+
+    Redis().set(key=str(user.id), value=refresh_token)
+
+    response = jsonify(access_token=access_token, refresh_token=refresh_token)
+    set_access_cookies(response, access_token)
+
+    return response
 
 
 @user_bp.route('/refresh', methods=['POST'])
 @jwt_required(refresh=True)
 def refresh():
+    log.info('Refresh')
+
     identity = get_jwt_identity()
+    log.info(f'{identity=}')
+
+    redis = Redis()
+    refresh_token_in_db = redis.get(key=identity)
+    if not refresh_token_in_db:
+        abort(401, description='Not found refresh token in db')
+    k = request.cookies['refresh_token_cookie']
+    if refresh_token_in_db != k:
+        abort(401, description='Not correct refresh token in db')
+
     access_token = create_access_token(identity=identity, fresh=False)
-    return jsonify(access_token=access_token)
+    refresh_token = create_refresh_token(identity=identity)
+
+    Redis().set(key=identity, value=refresh_token)
+
+    response = jsonify('')
+    set_access_cookies(response, access_token)
+
+    return response
 
 
-@user_bp.route('/logout')
-@login_required
+@user_bp.route('/logout', methods=['POST'])
+@jwt_required(refresh=True)
+# @jwt_required()
 def logout():
-    logout_user()
+    identity = get_jwt_identity()
+
+    Redis().delete(key=identity)
     response = jsonify({'msg': 'logout successful'})
     unset_jwt_cookies(response)
 
     return response
 
 
-@user_bp.route('/change')
-@login_required
-def change():
+@user_bp.route('/update', methods=['POST'])
+@jwt_required()
+def update():
+    jwt_data = verify_jwt_in_request()
+    if not jwt_data:
+        abort(400, description='Request without email')
+
+    params = request.get_json()
+    email = params.get('email')
+    password = params.get('password')
+    if not email:
+        abort(400, description='Request without email')
+    if not password:
+        abort(400, description='Request without password')
+    if not validate_email(email):
+        abort(400, description='Not valid email')
+
+    user_id = get_jwt_identity()
+    try:
+        user_service_db.get_user_by_id(user_id=user_id, is_optional=False)
+    except NotFoundInDBError:
+        raise NotFoundError(message='User not found')
+
+    user_service_db.update_user(user_id, email, password)
+
     return 'change'
 
 
-@user_bp.route('/get_sessions')
+@user_bp.route('/get_sessions', methods=['GET'])
 @login_required
 def get_sessions():
     return 'get_sessions'
